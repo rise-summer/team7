2ea7e71068c6ebed2df6035210109c2d683dab52
Joshua Kaplan <joshuagraykaplan@gmail.com>
2020-07-18 12:35:43 -0500

    Hopefully finally fixed linting. Went with a simpler solution that requires local installation of node

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-#INSTALL
+## INSTALL
+
+install node
 install docker
 
+run `npm install`
 run `docker-compose up`
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
+/*
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+*/
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.end('hello world. nyaa');
+app.get("/", (req, res) => {
+  res.end("hello world");
 });
 
 app.listen(process.env.PORT);
